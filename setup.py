690293fc676fe952828f043ca5f0c214a98dac00
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-05-22 18:36:13 +0200

    black (#33)
    * black
    
    * fixup! Format Python code with black

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 
 setup(
-    name = 'millheater',
-    packages = ['mill'],
-    install_requires=['aiohttp>=3.0.6', 'async_timeout>=1.4.0'],
-    version='0.4.0',
-    description='A python3 library to communicate with Mill',
-    long_description='A python3 library to communicate with Mill',
-    python_requires='>=3.5.3',
-    author='Daniel Hjelseth Hoyer',
-    author_email='mail@dahoiv.net',
-    url='https://github.com/Danielhiversen/pymill',
+    name="millheater",
+    packages=["mill"],
+    install_requires=["aiohttp>=3.0.6", "async_timeout>=1.4.0"],
+    version="0.4.0",
+    description="A python3 library to communicate with Mill",
+    long_description="A python3 library to communicate with Mill",
+    python_requires=">=3.5.3",
+    author="Daniel Hjelseth Hoyer",
+    author_email="mail@dahoiv.net",
+    url="https://github.com/Danielhiversen/pymill",
     license="MIT",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Home Automation',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-        ]
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
